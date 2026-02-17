61b238a2e8ff414485e2299f0a88ef7219b3d706
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-02-17 15:35:38 +0200

    Completed part 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 import { useState } from 'react'
 
+function Header(props) {
+  console.log(props)
+  return(
+    <h1>{props.course}</h1>
+  );
+};
+
+function Content(props) {
+  console.log(props)
+  return(
+    <>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </>
+  )
+}
+
+function Total(props) {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
   const course = 'Half Stack application development'
@@ -10,23 +34,17 @@ function App() {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  
+
   return (
     <>
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1}
+       part2={part2} exercises2={exercises2} 
+       part3={part3} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
-  
+    
     </>
   )
 }
