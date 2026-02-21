8d8fb613a11e7013d76d3b0a64797e27b8b42c9f
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-02-22 01:34:41 +0200

    Rendering from objects...need to simplify

--- src/App.jsx
+++ src/App.jsx
@@ -37,93 +37,26 @@ function Total(props) {
 function App() {
   const [count, setCount] = useState(0)
 
-  //Objects
-  const profile1 = {
-    name: "Percy",
-    camp: "Camp Half Blood",
-    parent: "Poseidon"
-  }
-
-  const profile2 = {
-    name: "Jason",
-    camp: "Camp Jupiter",
-    parent: "Jupiter"
-  }
-  const profile3 = {
-    name: "Magnus",
-    camp: "Hotel Valhalla",
-    parent: "Frey"
-  }
-
-  // const profiles = [profile1, profile2, profile3]
-
-  // profiles.forEach((profile) => {
-  //   console.log(profile.name)
-  //   console.log(profile.camp)
-  //   console.log(profile.parent)
-  // })
-
-  const names = [profile1.name, profile2.name, profile3.name]
-  const camps = [profile1.camp, profile2.camp, profile3.camp]
-  const parents = [profile1.parent, profile2.parent, profile3.parent]
-
-  // printing out the ordered way
-  // names.forEach((name) => {
-  //   console.log(name)
-  // })
-
-  // camps.forEach((camp) => {
-  //   console.log(camp)
-  // })
-  
-  // parents.forEach((parent) => {
-  //   console.log(parent)
-  // })
-
-  const orderedArray = [names, camps, parents]
-  orderedArray.forEach((element) => {
-    console.log(element)
-  })
-
-  // Arrays
-  const fruits = ['apples', 'pears', 'bananas', 'jfruit', 'dfruit'];
-
-  const [first, second, ...rest]  = fruits
-
-  console.log(first)
-  console.log(second)
-  console.log(rest)
-
-  fruits.forEach((fruit) => {
-    console.log(fruit)
-  })
-
-  const fruits2 = fruits.map(value => '<p>' + value + '</p>');
-  fruits2.forEach((fruit2) => {
-    console.log(fruit2)
-  })
-
 
   const course = 'Half Stack application development'
   // Creating Objects
   // simplifying these objects
-  const parts = [
-  {
+  const part1 = {
     name: 'Fundamentals of React',
     exercises: 10
-  },
+  }
 
-  { 
+  const part2 = { 
     name:'Using props to pass data', 
     exercises: 7
-  },
+  }
 
 
-  {
+  const part3 = {
     name: 'State of a component', 
     exercises:14
   }
-  ]
+  
 
 
 
@@ -132,8 +65,8 @@ function App() {
     <>
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </div>
     
     </>
